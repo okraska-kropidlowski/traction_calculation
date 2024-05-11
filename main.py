a22723fcf0ef184040d2ed223e7259009b0dfaa1
okraska-kropidlowski <wojtek.1986@gmail.com>
2024-05-11 17:01:57 +0200

    First calculations

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ time = 0
 acceleration = float(input("Provide the vehicle acceleration [m/s²]: "))
 time_step = float(input("Provide the calculation time step [s]: "))
 overall_time = float(input("Provide the total calculation time [s]: "))
+
+while time < overall_time:
+    speed = speed + (acceleration * time_step)
+    speed_2f = ("{:.2f}".format(speed))
+    print("Current speed: " + str(speed_2f) + " m/s²")
+    time = time + time_step
+    sleep(0.1)
