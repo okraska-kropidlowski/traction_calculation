72286e375e4868194fe7fa618966ae5f32021a58
okraska-kropidlowski <121766018+okraska-kropidlowski@users.noreply.github.com>
2024-05-11 16:19:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+acceleration = 0
+speed = 0
+travelled_distance = 0
+time = 0
+
+acceleration = float(input("Provide the vehicle acceleration [m/s²]: "))
+time_step = float(input("Provide the calculation time step [s]: "))
