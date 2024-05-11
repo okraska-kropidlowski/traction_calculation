85ccded6aeb3207727e951268020a5cb93edb8bf
okraska-kropidlowski <wojtek.1986@gmail.com>
2024-05-11 17:26:25 +0200

    Speed calculation done

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from time import sleep
 
-acceleration = 0
-speed = 0
-travelled_distance = 0
 time = 0
+speed = 0
 
 acceleration = float(input("Provide the vehicle acceleration [m/s²]: "))
 time_step = float(input("Provide the calculation time step [s]: "))
