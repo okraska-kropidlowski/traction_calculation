b17fa1ebe15a203d2cc677f4166f2d39dd7c727b
okraska-kropidlowski <wojtek.1986@gmail.com>
2024-05-12 11:50:23 +0200

    Speed displaying in km/h

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ time_step = float(input("Provide the calculation time step [s]: "))
 overall_time = float(input("Provide the total calculation time [s]: "))
 
 while time < overall_time:
+
+    #CALCULATIONS
     time = time + time_step
     speed = speed + (acceleration * time_step)
+
+    #FORMATTING
+    speed_kmh = speed * 3.6
     time_0f = ("{:.0f}".format(time))
-    speed_2f = ("{:.2f}".format(speed))
+    speed_2f = ("{:.2f}".format(speed_kmh))
+
+    #DISPLAYING
     print("Time point: " + str(time_0f) + " s")
-    print("Current speed: " + str(speed_2f) + " m/s²")
+    print("Current speed: " + str(speed_2f) + " km/h")
     sleep(0.5)
