5dc1b35a0c7f07d2df610f0fe1f6f87197a9aba0
okraska-kropidlowski <wojtek.1986@gmail.com>
2024-05-12 11:45:42 +0200

    Adding travelled distance

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from time import sleep
 
 time = 0
 speed = 0
+distance = 0
 
 acceleration = float(input("Provide the vehicle acceleration [m/s²]: "))
 time_step = float(input("Provide the calculation time step [s]: "))
 overall_time = float(input("Provide the total calculation time [s]: "))
 
 while time < overall_time:
-    speed = speed + (acceleration * time_step)
-    speed_2f = ("{:.2f}".format(speed))
-    print("Current speed: " + str(speed_2f) + " m/s²")
     time = time + time_step
-    sleep(0.1)
+    speed = speed + (acceleration * time_step)
+    time_0f = ("{:.0f}".format(time))
+    speed_2f = ("{:.2f}".format(speed))
+    print("Time point: " + str(time_0f) + " s")
+    print("Current speed: " + str(speed_2f) + " m/s²")
+    sleep(0.5)
