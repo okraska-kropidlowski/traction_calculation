435f854c8336a749ecb467b545fbae0bb35e668f
okraska-kropidlowski <wojtek.1986@gmail.com>
2024-05-18 21:02:45 +0200

    Added power at wheel

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ acceleration = float(input("Provide the vehicle acceleration [m/s²]: "))
 time_step = float(input("Provide the calculation time step [s]: "))
 overall_time = float(input("Provide the total calculation time [s]: "))
 max_speed = min(parameters.speed_max, ((parameters.power_max / tractive_effort) * 3.6))
-print("Speed limit: " + max_speed + "km/h")
+print("Speed limit: " + str(max_speed) + "km/h")
 
 while time < overall_time:
 
@@ -30,15 +30,20 @@ while time < overall_time:
     rotational_speed = (speed * 60) / (2 * math.pi * (parameters.wheel_diameter / 1000))
     rotational_speed_motor = rotational_speed * parameters.gear_ratio
 
+    #POWER AT WHEEL [#kW]
+    power_at_wheel = tractive_effort * (speed / 3.6)
+
     #FORMATTING
     time_0f = ("{:.0f}".format(time))
     speed_1f = ("{:.1f}".format(speed))
     distance_2f = ("{:.2f}".format(distance))
     rotational_speed_motor_2f = ("{:.2f}".format(rotational_speed_motor))
+    power_at_wheel_0f = ("{:.0f}".format(power_at_wheel))
     
     #DISPLAYING
     print("Time point: " + str(time_0f) + " s")
     print("Current speed: " + str(speed_1f) + " km/h")
     print("Travelled distance: " + str(distance_2f) + " m")
     print("Motor rotational speed: " + str(rotational_speed_motor_2f) + " rpm")
+    print("Power at wheel: " + str(power_at_wheel_0f) + " kW")
     sleep(0.5)
